143f953a171bcf886547a06b0d28653b1ace50bc
0Xero7 <smpsmp13@gmail.com>
2024-11-22 22:51:52 +0530

    initial commit

---
+++ limitless.go
@@ -0,0 +1,346 @@
+package limitless
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+type Limitless struct {
+	ddb *dynamodb.Client
+}
+
+func NewClient(dynamodb *dynamodb.Client) *Limitless {
+	client := new(Limitless)
+	client.ddb = dynamodb
+	return client
+}
+
+func GetItem[T any](l *Limitless, tableName string, key *T) (*T, error) {
+	nout := new(T)
+
+	Key, err := attributevalue.MarshalMap(key)
+	if err != nil {
+		return nil, err
+	}
+	res, err := l.ddb.GetItem(context.TODO(), &dynamodb.GetItemInput{
+		TableName: &tableName,
+		Key:       Key,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(res.Item) == 0 {
+		return nil, nil
+	}
+
+	if err := attributevalue.UnmarshalMap(res.Item, &nout); err != nil {
+		return nil, err
+	}
+	return nout, nil
+}
+
+func PutItem[T any](l *Limitless, tableName string, item T) error {
+	Item, err := attributevalue.MarshalMap(item)
+	if err != nil {
+		return err
+	}
+	_, err = l.ddb.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: &tableName,
+		Item:      Item,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PutItemConditional[T any](l *Limitless, tableName string, conditionExpression string, item T) error {
+	Item, err := attributevalue.MarshalMap(item)
+	if err != nil {
+		return err
+	}
+	_, err = l.ddb.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName:           &tableName,
+		Item:                Item,
+		ConditionExpression: &conditionExpression,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func BatchPutItem[T any](l *Limitless, tableName string, items []T) error {
+	writeRequests := make([]types.WriteRequest, len(items))
+	for i, v := range items {
+		Item, err := attributevalue.MarshalMap(v)
+		if err != nil {
+			return err
+		}
+
+		writeRequests[i] = types.WriteRequest{
+			PutRequest: &types.PutRequest{
+				Item: Item,
+			},
+		}
+	}
+
+	_, err := l.ddb.BatchWriteItem(context.TODO(), &dynamodb.BatchWriteItemInput{
+		RequestItems: map[string][]types.WriteRequest{
+			tableName: writeRequests,
+		},
+	})
+	return err
+}
+
+func BatchGetItem[T any](l *Limitless, tableName string, items []T) ([]T, error) {
+	keys := make([]map[string]types.AttributeValue, len(items))
+	for i, v := range items {
+		Item, err := attributevalue.MarshalMap(v)
+		if err != nil {
+			return nil, err
+		}
+
+		keys[i] = Item
+	}
+
+	response, err := l.ddb.BatchGetItem(context.TODO(), &dynamodb.BatchGetItemInput{
+		RequestItems: map[string]types.KeysAndAttributes{
+			tableName: {
+				Keys: keys,
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]T, 0)
+	for _, r := range response.Responses {
+		for _, v := range r {
+			item := new(T)
+			err = attributevalue.UnmarshalMap(v, item)
+			if err != nil {
+				return nil, err
+			}
+			result = append(result, *item)
+		}
+	}
+	return result, err
+}
+
+type ScanRequest struct {
+	TableName         string
+	IndexName         *string
+	ConsistentRead    *bool
+	Limit             *int32
+	ExclusiveStartKey map[string]types.AttributeValue
+}
+
+type QueryRequest struct {
+	TableName                string
+	Condition                string
+	Values                   map[string]any
+	IndexName                *string
+	ConsistentRead           *bool
+	Ascending                *bool
+	Limit                    *int32
+	ProjectionExpression     *string
+	ExpressionAttributeNames map[string]string
+	ExclusiveStartKey        map[string]types.AttributeValue
+}
+
+func Query[T any](l *Limitless, queryRequest QueryRequest) ([]T, map[string]types.AttributeValue, error) {
+	condition := queryRequest.Condition
+	values := queryRequest.Values
+
+	keyNameList := make([]string, 0)
+	valueNameList := make([]string, 0)
+	parsedCondition := ""
+	for i := 0; i < len(condition); i = i + 1 {
+		if condition[i] != ':' && condition[i] != '#' {
+			parsedCondition += string(condition[i])
+			continue
+		} else {
+			keyName := ""
+			isKeyName := false
+			if condition[i] == '#' {
+				isKeyName = true
+			}
+
+			for j := i; j < len(condition) && condition[j] != ' '; j += 1 {
+				keyName += string(condition[j])
+				i += 1
+			}
+			if isKeyName {
+				keyNameList = append(keyNameList, keyName)
+			} else {
+				valueNameList = append(valueNameList, keyName)
+			}
+			i = i - 1
+		}
+	}
+
+	expressionAttributeNames := make(map[string]string)
+	for i, k := range keyNameList {
+		replacedKey := "#KEY" + fmt.Sprintf("%d", i)
+		expressionAttributeNames[replacedKey] = strings.TrimPrefix(k, "#")
+		condition = strings.ReplaceAll(condition, k, replacedKey)
+	}
+	ExpressionAttributeValues := make(map[string]any)
+	for i, k := range valueNameList {
+		replacedKey := ":VALUE" + fmt.Sprintf("%d", i)
+		ExpressionAttributeValues[replacedKey] = values[k]
+		condition = strings.ReplaceAll(condition, k, replacedKey)
+	}
+
+	valueMap, err := attributevalue.MarshalMap(ExpressionAttributeValues)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Copy user send ExpressionAttributeNames
+	for k, v := range queryRequest.ExpressionAttributeNames {
+		expressionAttributeNames[k] = v
+	}
+	queryInput := new(dynamodb.QueryInput)
+	queryInput.TableName = &queryRequest.TableName
+	queryInput.KeyConditionExpression = &condition
+	queryInput.ExpressionAttributeNames = expressionAttributeNames
+	queryInput.ExpressionAttributeValues = valueMap
+	queryInput.ConsistentRead = queryRequest.ConsistentRead
+	queryInput.IndexName = queryRequest.IndexName
+	queryInput.Limit = queryRequest.Limit
+	queryInput.ScanIndexForward = queryRequest.Ascending
+	queryInput.ExclusiveStartKey = queryRequest.ExclusiveStartKey
+	queryInput.ProjectionExpression = queryRequest.ProjectionExpression
+
+	result, err := l.ddb.Query(context.TODO(), queryInput)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	res := make([]T, 0)
+	for _, k := range result.Items {
+		temp := new(T)
+		err := attributevalue.UnmarshalMap(k, temp)
+		if err != nil {
+			return nil, nil, err
+		}
+		res = append(res, *temp)
+	}
+	return res, result.LastEvaluatedKey, nil
+}
+
+type PutItemRequest struct {
+	TableName string
+	Item      interface{}
+}
+
+func TransactWriteItems(l *Limitless, items *[]PutItemRequest, idempotencyToken *string) error {
+	transactItems := make([]types.TransactWriteItem, 0)
+	for _, k := range *items {
+		temp, err := attributevalue.MarshalMap(k.Item)
+		if err != nil {
+			return err
+		}
+
+		transactItems = append(transactItems, types.TransactWriteItem{
+			Put: &types.Put{
+				TableName: &k.TableName,
+				Item:      temp,
+			},
+		})
+	}
+
+	_, err := l.ddb.TransactWriteItems(context.TODO(), &dynamodb.TransactWriteItemsInput{
+		TransactItems:      transactItems,
+		ClientRequestToken: idempotencyToken,
+	})
+	if err != nil {
+		fmt.Println("Error = ", err)
+	}
+	return err
+}
+
+func UpdateItem[T any](l *Limitless, tableName string, key *T, item *T) error {
+	value := reflect.ValueOf(*item)
+	n := value.NumField()
+	fields := make(map[string]string)
+	values := make(map[string]any)
+	updateExpressions := make([]string, 0)
+	for i := range n {
+		fieldName := strings.Split(value.Type().Field(i).Tag.Get("dynamodbav"), ",")[0]
+		if value.Field(i).IsZero() {
+			continue
+		}
+		fieldValue := value.Field(i).Interface()
+		I := fmt.Sprintf("%d", i)
+
+		fields["#KEY"+I] = fieldName
+		values[":VALUE"+I] = fieldValue
+		updateExpressions = append(updateExpressions, "#KEY"+I+" = "+":VALUE"+I)
+	}
+	updateExpression := "SET " + strings.Join(updateExpressions, ",")
+	keyMap, _ := attributevalue.MarshalMap(*key)
+	fieldValues, _ := attributevalue.MarshalMap(values)
+
+	_, err := l.ddb.UpdateItem(context.TODO(), &dynamodb.UpdateItemInput{
+		TableName:                 &tableName,
+		Key:                       keyMap,
+		UpdateExpression:          &updateExpression,
+		ExpressionAttributeNames:  fields,
+		ExpressionAttributeValues: fieldValues,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func DeleteItem[T any](l *Limitless, tableName string, key *T) error {
+	Key, err := attributevalue.MarshalMap(key)
+	if err != nil {
+		return err
+	}
+
+	_, err = l.ddb.DeleteItem(context.TODO(), &dynamodb.DeleteItemInput{
+		TableName: &tableName,
+		Key:       Key,
+	})
+	return err
+}
+
+func Scan[T any](l *Limitless, request ScanRequest) ([]T, map[string]types.AttributeValue, error) {
+	resp, err := l.ddb.Scan(context.TODO(), &dynamodb.ScanInput{
+		TableName:         &request.TableName,
+		IndexName:         request.IndexName,
+		ConsistentRead:    request.ConsistentRead,
+		Limit:             request.Limit,
+		ExclusiveStartKey: request.ExclusiveStartKey,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	lek := resp.LastEvaluatedKey
+	out := make([]T, 0)
+
+	for _, v := range resp.Items {
+		item := new(T)
+		if attributevalue.UnmarshalMap(v, item) != nil {
+			return nil, nil, err
+		}
+		out = append(out, *item)
+	}
+
+	return out, lek, nil
+}
